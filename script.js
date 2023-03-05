855e3d6333a9325f590d9f1c5b55142f6106627c
apenubol26 <126985271+apenubol26@users.noreply.github.com>
2023-03-04 22:18:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,93 @@
-function showTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
+@import body {
+    background-color: powderblue;
+  }
+body
+    {
+    width: 100%;
+    height: 100%;
+    background-image:url(https://img.lovepik.com/photo/40010/2650.jpg_wh300.jpg);
+    background-size: cover;
+    background-attachment:fixed;
+    margin: 0;
+    padding: 0;
     }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
-}
 
-showTime();
+  h1 {
+    color: #36596c;
+    text-align: center;
+    font-family: "Lucida Console",monospace;
+  }
+  h2{
+    color: #36596c;
+    text-align: center;
+    font-family: "Lucida Console",monospace;
+  }
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #6899b4;
+  }
+  
+  li {
+    float: left;
+  }
+  
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px 16px;
+    text-decoration: none;
+  }
+  
+  li a:hover {
+    background-color: #c1dcec;
+  }
+
+  ul div {
+    display: inline-block;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+url(https://fonts.googleapis.com/css?family=Khula:700);
+body {
+  background: #111;
+}
+.hidden {
+  opacity:0;
+}
+.console-container {
+ 
+  font-family:Khula;
+  font-size:4em;
+  text-align:center;
+  height:200px;
+  width:600px;
+  display:block;
+  position:center;
+  color:white;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+  margin:auto;
+}
+.console-underscore {
+   display:inline-block;
+  position:relative;
+  top:-0.14em;
+  left:10px;
+}
