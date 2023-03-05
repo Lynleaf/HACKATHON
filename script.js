d0bdb18db6cac769b596fc7c82675e261c90fc6e
apenubol26 <126985271+apenubol26@users.noreply.github.com>
2023-03-04 22:20:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,50 @@
-@import body {
-    background-color: powderblue;
-  }
-body
-    {
-    width: 100%;
-    height: 100%;
-    background-image:url(https://img.lovepik.com/photo/40010/2650.jpg_wh300.jpg);
-    background-size: cover;
-    background-attachment:fixed;
-    margin: 0;
-    padding: 0;
+consoleText(['Breathe', 'Relax', 'Close your eyes'], 'text',['lightblue','lightblue','lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
     }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
 
-  h1 {
-    color: #36596c;
-    text-align: center;
-    font-family: "Lucida Console",monospace;
-  }
-  h2{
-    color: #36596c;
-    text-align: center;
-    font-family: "Lucida Console",monospace;
-  }
+    } else {
+      con.className = 'console-underscore'
 
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #6899b4;
-  }
-  
-  li {
-    float: left;
-  }
-  
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 16px 16px;
-    text-decoration: none;
-  }
-  
-  li a:hover {
-    background-color: #c1dcec;
-  }
-
-  ul div {
-    display: inline-block;
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-url(https://fonts.googleapis.com/css?family=Khula:700);
-body {
-  background: #111;
-}
-.hidden {
-  opacity:0;
-}
-.console-container {
- 
-  font-family:Khula;
-  font-size:4em;
-  text-align:center;
-  height:200px;
-  width:600px;
-  display:block;
-  position:center;
-  color:white;
-  top:0;
-  bottom:0;
-  left:0;
-  right:0;
-  margin:auto;
-}
-.console-underscore {
-   display:inline-block;
-  position:relative;
-  top:-0.14em;
-  left:10px;
+      visible = true;
+    }
+  }, 400)
 }
